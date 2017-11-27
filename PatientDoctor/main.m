c0e11aaf41e5c373a0240f82f6148c6eaa7f691e
Varninder Rai <varninder@openmailbox.org>
2017-11-26 20:16:56 -0800

    Initial Commit

---
+++ PatientDoctor/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PatientDoctor
+//
+//  Created by Livleen Rai on 2017-11-26.
+//  Copyright © 2017 Livleen Rai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
